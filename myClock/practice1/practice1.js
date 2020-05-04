ca5bbc603c639d0b60b819e2768bdf283007cec7
yechubot <61838900+yechubot@users.noreply.github.com>
2020-05-04 20:25:40 +0900

    Add files via upload

---
+++ myClock/practice1/practice1.js
@@ -0,0 +1,16 @@
+const tikTok = document.querySelector("h1");
+
+function getTime() {
+    const date = new Date();
+    const hours = date.getHours();
+    const minutes = date.getMinutes();
+    const seconds = date.getSeconds();
+    
+    tikTok.innerText = `${
+        hours < 10 ? `0${hours}` : hours}:${
+        minutes < 10 ? `0${minutes}` : minutes}:${
+        seconds < 10 ? `0${seconds}` : seconds}`;
+}
+
+getTime();
+setInterval(getTime,1000);
