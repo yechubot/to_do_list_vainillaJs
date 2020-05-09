edc990661e4da3cb11c2cc938cb8ae5138c97af7
yechubot <61838900+yechubot@users.noreply.github.com>
2020-05-10 01:32:06 +0900

    Update app.js

--- to_do_list/app.js
+++ to_do_list/app.js
@@ -1,6 +1,5 @@
-
+ 
 const list = document.querySelector('#to-dos ul');
-
 //delete lists
 list.addEventListener('click',function(e){
 
@@ -23,17 +22,50 @@ addList.addEventListener('submit',function(e){
     const createLi = document.createElement('li');
     const listName = document.createElement('span');
     const deleteButton = document.createElement('span');
+    const doneButton = document.createElement('span');
 
     // add content
     deleteButton.textContent = 'Delete';
+    doneButton.textContent = 'Done';
     listName.textContent = value;
+
 
     //add class
     listName.classList.add('lists');
     deleteButton.classList.add('deleteBtn');
+    doneButton.classList.add('doneBtn');
 
     //append to document
     createLi.appendChild(listName);
     createLi.appendChild(deleteButton);
+    createLi.appendChild(doneButton);
     list.appendChild(createLi);
 })
+
+//lists done
+
+const doneList = document.querySelector('#done ul');
+list.addEventListener('click',function(e){
+
+    if(e.target.className == 'doneBtn'){
+       
+        const li = e.target.parentElement;
+        doneList.appendChild(li);
+        
+        // hide done button when it moved to done! part..
+        
+
+        /*const classBtn = doneList.getElementsByClassName('doneBtn');
+        classBtn.style.display = 'none'; - why it doesn't work?! :( */ 
+    }
+});
+
+//delete done-lists
+doneList.addEventListener('click',function(e){
+
+    if(e.target.className == 'deleteBtn'){
+        const li = e.target.parentElement;
+        doneList.removeChild(li);
+    }
+
+});
