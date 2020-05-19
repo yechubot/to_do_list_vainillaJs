e252104c00ad0dbf8637f93ec8c5df92d005f431
yechubot <61838900+yechubot@users.noreply.github.com>
2020-05-19 21:17:42 +0900

    Add files via upload

---
+++ typewriter/app.js
@@ -0,0 +1,20 @@
+const texts = ['not sure', 'for sure', 'I want a pancake'];
+let count = 0;
+let index = 0;
+let currentText = '';
+let letter = '';
+
+(function type() {
+    if(count===texts.length){
+        count = 0;
+    }
+    currentText = texts[count];
+    letter = currentText.slice(0, ++index);
+
+    document.querySelector('.typing').textContent = letter;
+    if(letter.length === currentText.length){
+        count++;
+        index = 0;
+    }
+    setTimeout(type, 400);
+}()); // run immediately
